12f38517ea465c8e8c40db62d716284a43680c1c
brayanfloresf3 <brayanfloresf3@gmail.com>
2024-06-21 10:57:09 -0500

    hize paginacion en la page de chararters y modifique mas styles

--- modules/characters.js
+++ modules/characters.js
@@ -1,27 +1,41 @@
-const urlRickAndMorty = "https://rickandmortyapi.com/api/character/? + {pages}";
+const urlRickAndMorty = "https://rickandmortyapi.com/api/character/?page=";
 
-const { createApp } = Vue;
+        const { createApp } = Vue;
 
-createApp({
-  data() {
-    return {
-      characters: [],
-      
-    };
-  },
-  created() {
-    this.traerData(urlRickAndMorty);
-  },
-  methods: {
-    traerData(url) {
-      fetch(url)
-        .then(response => response.json())
-        .then(data => {
-          this.characters = data.results;
-          console.log(this.characters);
-        })
-        .catch(error => console.error('Error fetching data:', error));
-    },
-    
-  }
-}).mount('#app');
+        createApp({
+            data() {
+                return {
+                    characters: [],
+                    currentPage: 1,
+                    totalPages: 42
+                };
+            },
+            created() {
+                this.fetchData(this.currentPage);
+            },
+            methods: {
+                fetchData(page) {
+                    fetch(urlRickAndMorty + page)
+                        .then(response => response.json())
+                        .then(data => {
+                            this.characters = data.results;
+                            console.log(this.characters);
+                            console.log(urlRickAndMorty + page); 
+                            
+                        })
+                        .catch(error => console.error('Error fetching data:', error));
+                },
+                nextPage() {
+                    if (this.currentPage < this.totalPages) {
+                        this.currentPage++;
+                        this.fetchData(this.currentPage);
+                    }
+                },
+                prevPage() {
+                    if (this.currentPage > 1) {
+                        this.currentPage--;
+                        this.fetchData(this.currentPage);
+                    }
+                }
+            }
+        }).mount('#app');
