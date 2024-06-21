688960a70216edd6d18f1a5b578552604e220a29
JESUS FERNANDEZ <fernandezromerojesusernesto27@gmail.com>
2024-06-21 12:07:27 -0500

    .

---
+++ modules/locations.js
@@ -0,0 +1,47 @@
+let urlBase = "https://rickandmortyapi.com/api/location/";
+
+const app = Vue.createApp({
+    data() {
+        return {
+            ubicaciones: [],
+            filtroUbicacion: '',
+            personajesFiltrados: [],
+        };
+    },
+    created() {
+        this.traerData();
+    },
+    methods: {
+        traerData() {
+            fetch(urlBase)
+                .then(response => response.json())
+                .then(data => {
+                    this.ubicaciones = data.results;
+                });
+        },
+        filtrarPersonajesPorUbicacion() {
+            if (!this.filtroUbicacion) {
+                this.personajesFiltrados = [];
+                return;
+            }
+
+            fetch(`${urlBase}${this.filtroUbicacion}`)
+                .then(response => response.json())
+                .then(data => this.obtenerDetallesPersonajes(data.residents))
+                .then(residentes => {
+                    this.personajesFiltrados = residentes;
+                });
+        },
+        obtenerDetallesPersonajes(residentes) {
+            const detallesPromesas = residentes.map(url => fetch(url).then(response => response.json()));
+            return Promise.all(detallesPromesas);
+        },
+    },
+    computed: {
+        ubicacionSeleccionada() {
+            return this.ubicaciones.find(ubicacion => ubicacion.id === this.filtroUbicacion);
+        },
+    },
+});
+
+app.mount('#app');
