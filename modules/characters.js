a840e4d8959df8251528521956b5d7db0bd01a98
brayanfloresf3 <brayanfloresf3@gmail.com>
2024-06-24 11:19:54 -0500

    realice los filtros por list y por texto

--- modules/characters.js
+++ modules/characters.js
@@ -1,44 +1,76 @@
 const urlRickAndMorty = "https://rickandmortyapi.com/api/character/?page=";
 
-        const { createApp } = Vue;
+const { createApp } = Vue;
 
-        createApp({
-            data() {
-                return {
-                    characters: [],
-                    currentPage: 1,
-                    totalPages: 42,
-                    searchText: ""
-                };
-            },
-            created() {
-                this.fetchData(this.currentPage);
-            },
-            methods: {
-                fetchData(page) {
-                    fetch(urlRickAndMorty + page)
-                        .then(response => response.json())
-                        .then(data => {
-                            this.characters = data.results;
-                            console.log(this.characters);
-                            console.log(urlRickAndMorty + page); 
-                            console.log(searchText);
-                            
-                            
-                        })
-                        .catch(error => console.error('Error fetching data:', error));
-                },
-                nextPage() {
-                    if (this.currentPage < this.totalPages) {
-                        this.currentPage++;
-                        this.fetchData(this.currentPage);
-                    }
-                },
-                prevPage() {
-                    if (this.currentPage > 1) {
-                        this.currentPage--;
-                        this.fetchData(this.currentPage);
-                    }
-                }
-            }
-        }).mount('#app');
+createApp({
+  data() {
+    return {
+      characters: [],
+      currentPage: 1,
+      totalPages: 42,
+      searchText: '',
+      currentStatusFilter: '',
+      currentSpeciesFilter: '',
+      currentGenderFilter: ''
+    };
+  },
+  created() {
+    this.fetchAllCharacters();
+  },
+  computed: {
+    filteredCharacters() {
+      let filteredCharacters = this.characters;
+
+      if (this.currentStatusFilter) {
+        filteredCharacters = filteredCharacters.filter(character => character.status === this.currentStatusFilter);
+      }
+      if (this.currentSpeciesFilter) {
+        filteredCharacters = filteredCharacters.filter(character => character.species === this.currentSpeciesFilter);
+      }
+      if (this.currentGenderFilter) {
+        filteredCharacters = filteredCharacters.filter(character => character.gender === this.currentGenderFilter);
+      }
+
+      if (this.searchText) {
+        filteredCharacters = filteredCharacters.filter(character =>
+          character.name.toLowerCase().includes(this.searchText.toLowerCase())
+        );
+      }
+
+      return filteredCharacters;
+    }
+  },
+  methods: {
+    fetchAllCharacters() {
+      this.fetchData(this.currentPage);
+    },
+    fetchData(page) {
+      fetch(urlRickAndMorty + page)
+        .then(response => response.json())
+        .then(data => {
+
+          this.characters = this.characters.concat(data.results);
+
+
+          if (page < this.totalPages) {
+            this.fetchData(page + 1);
+          }
+
+
+        })
+        .catch(error => console.error('Error fetching data:', error));
+    },
+    searchInput() {
+
+    },
+    filterByStatus(status) {
+      this.currentStatusFilter = status;
+    },
+    filterBySpecies(species) {
+      this.currentSpeciesFilter = species;
+    },
+    filterByGender(gender) {
+      this.currentGenderFilter = gender;
+    }
+  }
+}).mount('#app');
