3a3e2530edfd2a36bf75df086f99d5175988e929
MATEO_RODRIGUEZ <20mateorodriguez03@gmail.com>
2024-06-25 11:23:48 -0300

    agregado de filtros

--- modules/episodes.js
+++ modules/episodes.js
@@ -1,32 +1,42 @@
-let urlRickAndMorty = "https://rickandmortyapi.com/api/episode";
+let urlRickAndMorty = "https://rickandmortyapi.com/api/episode?page=";
 
 const { createApp } = Vue;
 
 const app = createApp({
 
-    data(){
-        return{
+    data() {
+        return {
             productos: [],
-            categorias:[],
-            texto:"",
-            categoriasSeleccionadas:[]
+            productosBK:[],
+            temporadas: [],
+            filtroTexto: "",
+            categoriasSeleccionadas: [],
+            paginas: 3,
         }
     },
-    created(){
+    created() {
         this.traerData(urlRickAndMorty)
     },
-    methods:{
-        traerData(url){
-            fetch(url)
-            .then(response => response.json())
-            .then(data => {
-                console.log(data.results);
-                this.productos = data.results
-            })
+    methods: {
+        traerData(url, url2, url3) {
+            fetch(url, url2, url3)
+                .then(response => response.json())
+                .then(data => {
+                    console.log(data);
+                    this.productos = data.results;
+                    this.productosBK = data.results;
+                    console.log(this.productos);
+                    this.temporadas = Array.from(new Set(this.productos.map((product) => product.episode.slice(0, 3))))
+                    console.log(this.temporadas);
+                })
         }
     },
-    computed:{
-
+    computed: {
+        superFiltro() {
+            let filtroText = this.productosBK.filter(producto => producto.name.toLowerCase().includes(this.filtroTexto.toLowerCase()))
+            this.productos = filtroText
+            
+        }
     },
 
 }).mount('#app')
